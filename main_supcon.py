9ac03a9c99dc90258aed21573cd8347f64183be7
Omarraita <71269935+Omarraita@users.noreply.github.com>
2021-03-22 10:47:10 +0100

    Update main_supcon.py

--- main_supcon.py
+++ main_supcon.py
@@ -144,7 +144,8 @@ def set_loader(opt):
     normalize = transforms.Normalize(mean=mean, std=std)
 
     train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
+        #transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
+        transforms.Resize(size=opt.size, scale=(0.2, 1.))
         transforms.RandomHorizontalFlip(),
         transforms.RandomApply([
             transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
