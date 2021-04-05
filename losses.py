0733a89f5a036678b441c3e43d4d5324118566be
Omarraita <71269935+Omarraita@users.noreply.github.com>
2021-04-05 18:32:24 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,98 +1,78 @@
-"""
-Author: Yonglong Tian (yonglong@mit.edu)
-Date: May 07, 2020
-"""
-from __future__ import print_function
-
+from torchvision.transforms import transforms
+from torchvision import datasets
+from torch.utils.data import Dataset, DataLoader
+from gaussian_blur import GaussianBlur
+from view_generator import ContrastiveLearningViewGenerator
+import numpy as np 
 import torch
-import torch.nn as nn
+from PIL import Image
 
-
-class SupConLoss(nn.Module):
-    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR"""
-    def __init__(self, temperature=0.07, contrast_mode='all',
-                 base_temperature=0.07):
-        super(SupConLoss, self).__init__()
-        self.temperature = temperature
-        self.contrast_mode = contrast_mode
-        self.base_temperature = base_temperature
-
-    def forward(self, features, labels=None, mask=None):
-        """Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
-
-        Args:
-            features: hidden vector of shape [bsz, n_views, ...].
-            labels: ground truth of shape [bsz].
-            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
-                has the same class as sample i. Can be asymmetric.
-        Returns:
-            A loss scalar.
+class CardioSimCLRDataset(Dataset):
+    
+    """Dataset of images. Returns unlabelled dataset for contrastive learning"""
+    def __init__(self, npz_file, transform=None, to_tensor=False, to_normalize = False):
         """
-        device = (torch.device('cuda')
-                  if features.is_cuda
-                  else torch.device('cpu'))
+        Args:
+            npz_file (string): Path to the npz file with patches and labels.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.patches, self.labels = load_data(npz_file)
+        self.transform = transform
+        self.tensor = to_tensor
+        self.norm = to_normalize
 
-        if len(features.shape) < 3:
-            raise ValueError('`features` needs to be [bsz, n_views, ...],'
-                             'at least 3 dimensions are required')
-        if len(features.shape) > 3:
-            features = features.view(features.shape[0], features.shape[1], -1)
+    def __len__(self):
+        return len(self.labels)
 
-        batch_size = features.shape[0]
-        if labels is not None and mask is not None:
-            raise ValueError('Cannot define both `labels` and `mask`')
-        elif labels is None and mask is None:
-            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None:
-            labels = labels.contiguous().view(-1, 1)
-            if labels.shape[0] != batch_size:
-                raise ValueError('Num of labels does not match num of features')
-            mask = torch.eq(labels, labels.T).float().to(device)
-        else:
-            mask = mask.float().to(device)
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        image = self.patches[idx]
+        img = Image.fromarray(image)
+        label = np.array([self.labels[idx]])
+        
+        if self.transform is not None:
+            image = self.transform(img)
+            
+        if self.tensor==True : 
+            trsf = transforms.ToTensor()
+            image = trsf(image)/255
+            
+        if self.norm==True :         
+            trsf2 = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            image = trsf2(image)
+            
+        label = torch.from_numpy(label)
+        
+        sample = (image, label) 
+    
+        return sample
 
-        contrast_count = features.shape[1]
-        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
-        if self.contrast_mode == 'one':
-            anchor_feature = features[:, 0]
-            anchor_count = 1
-        elif self.contrast_mode == 'all':
-            anchor_feature = contrast_feature
-            anchor_count = contrast_count
-        else:
-            raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
+def get_simclr_transform(size, s=1):
+        """Return a set of data augmentation transformations as described in the SimCLR paper."""
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+        data_transforms = transforms.Compose([transforms.Resize(size=size),
+                                              transforms.RandomHorizontalFlip(),
+                                              #transforms.RandomApply([color_jitter], p=0.8),
+                                              #transforms.RandomGrayscale(p=0.2),
+                                              GaussianBlur(kernel_size=int(0.1 * size)),
+                                              transforms.ToTensor(),
+                                              normalize])
+        return data_transforms
 
-        # compute logits
-        anchor_dot_contrast = torch.div(
-            torch.matmul(anchor_feature, contrast_feature.T),
-            self.temperature)
-        # for numerical stability
-        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
-        logits = anchor_dot_contrast - logits_max.detach()
+def load_data(file_path): 
+    
+    """Load training data from file in ``.npz`` format."""
+    f = np.load(file_path, allow_pickle=True)
+    X, Y = f['X'], f['Y']
+    Y=np.squeeze(Y)
+    return (X,Y)
 
-        # tile mask
-        mask = mask.repeat(anchor_count, contrast_count)
-        # mask-out self-contrast cases
-        logits_mask = torch.scatter(
-            torch.ones_like(mask),
-            1,
-            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
-            0
-        )
-        mask = mask * logits_mask
-
-        # compute log_prob
-        exp_logits = torch.exp(logits) * logits_mask
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
-
-        # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
-
-        # loss
-        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
-        loss = loss.view(anchor_count, batch_size).mean()
-
-        return loss
+def get_cardio_smclr(train_file):
+    
+    train_data = CardioSimCLRDataset(train_file, transform = ContrastiveLearningViewGenerator(get_simclr_transform(96), 2))
+    
+    return train_data
